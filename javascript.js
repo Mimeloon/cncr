3d08e29612cbbebf3481b1335927138242d1e18e
Mimeloon <35579626+Mimeloon@users.noreply.github.com>
2018-01-18 22:05:39 +0100

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,81 @@
+function() {
+	var questions = {
+		question: "What is NOT a horse riding discipline?",
+		choices:{
+			correct: "Sitting",
+			wrong: ["Dressage", "Reining", "Show Jumping"]
+		}
+	},
+
+	{
+		question: "What is a castrated horse called?",
+		choices:{
+			correct: "Gelding",
+			wrong: ["Stallion", "Castrated horse", "A castrate"]
+
+	},
+
+	{
+		question: "What is the correct gait order from slowest to fastest?",
+		choices:{
+			correct: "Walk, Trot, Canter, Gallop",
+			wrong: ["Trot, Walk, Gallop, Canter", "Walk, Canter, Trot, Callop", "Canter, Walk, Gallop, Trot"]
+
+	},
+
+	{
+		question: "What is the name of the strap that goes under the horse’s belly so the saddle doesn’t move?",
+		choices:{
+			correct: "Girth",
+			wrong: ["Belly strap", "Horse belt"]
+
+	},
+
+	{
+		question: "What is the largest horse breed?",
+		choices:{
+			correct: "Shire",
+			wrong: ["Friesian", "Clydesdale", "Belgian Draft Horse"]
+
+	},
+
+	{
+		question: "How can you tell that horse is angry?",
+		choices:{
+			correct: "Ears pinned back",
+			wrong: ["Growling", "Licking it's lips"]
+
+	},
+
+	{
+		question: "What unit is most commonly used to measure a horses height?",
+		choices:{
+			correct: "Hands",
+			wrong: ["Centimeters", "Inches", "Feet"]
+
+	},
+
+	{
+		question: "What do you call the person that takes care and trims the horse’s hooves?",
+		choices:{
+			correct: "Farrier",
+			wrong: ["Hoof carer", "Hoof trimmer", "Hoover"]
+
+	},
+
+	{
+		question: "What is the proper name for the piece of equipment you put on the horses head and use it to steer with?",
+		choices:{
+			correct: "Bridle",
+			wrong: ["Head piece", "Head gear", "The steer"]
+
+	},
+
+	{
+		question: "What are dapples?",
+		choices:{
+			correct: "Round, coloured markings on the horse",
+			wrong: ["Noises from a horse’s stomach", "A British term for a halter"]
+
+	}
+}];	
